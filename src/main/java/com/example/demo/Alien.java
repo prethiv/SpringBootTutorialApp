f5903312fd054d98a201feda3f18076a02577258
Prethiv <prethiv191@gmail.com>
2021-05-14 18:57:20 +0530

    Component and Scopes defined

--- src/main/java/com/example/demo/Alien.java
+++ src/main/java/com/example/demo/Alien.java
@@ -3,6 +3,17 @@ package com.example.demo;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+//Component annotation tells spring to create a bean (object) inside a spring container
+//By default spring is implemented as singeton object provider 
+//it means for every component annotatin only one bean gets created inside spring container
+//even if we try to get one using context.getBean we wont get one
+//To overcome this thing we can use scope annotation by defining scope 
+//we can have multiple bean instance created inside a spring container.
+//Also if we did not use scope annitation by default spring will create a object 
+//for us no matter if we use it or not
+//But if we put scope spring will create a bean instance only on the request.
+
+
 @Component
 @Scope(value="prototype")
 public class Alien {
