dc19683bf77eed1230a72d743ddc174cc96a6706
Prethiv <prethiv191@gmail.com>
2021-05-14 18:04:23 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class Alien {
+	
+	public int aid;
+	public String name;
+	public String tech;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+
+}
